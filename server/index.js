1705819ea0bb041ced49ae9614557fc2c4f5ae9c
dinoxas <dhassiotis07@gmail.com>
2020-03-09 17:21:54 +0000

    Created helper functions for server-side socket to handle users and rooms

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+const router = require("./router");
+const PORT = process.env.PORT || 5000;
+
+// set up socket.io instance
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// client-side socket
+io.on("connection", socket => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) {
+      return callback(error);
+    }
+    // admin generated messages
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+    socket.join(user.room);
+
+    callback();
+  });
+
+  // user generated messages
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User has left!");
+  });
+});
+
+app.use(router);
+server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
